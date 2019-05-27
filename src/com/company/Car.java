4a3582d51d94aef2717f2971d53e2dce53094b8f
Nicholas <nich.otieno@gmail.com>
2019-05-27 10:37:53 +0300

    Add getters and setters

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -29,6 +29,94 @@ public class Car {
 
     }
 
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getMinSpeed() {
+        return minSpeed;
+    }
+
+    public void setMinSpeed(int minSpeed) {
+        this.minSpeed = minSpeed;
+    }
+
+    public int getNumberOfPeopleInCar() {
+        return numberOfPeopleInCar;
+    }
+
+    public void setNumberOfPeopleInCar(int numberOfPeopleInCar) {
+        this.numberOfPeopleInCar = numberOfPeopleInCar;
+    }
+
+    public int getMaxNumberOfPeople() {
+        return maxNumberOfPeople;
+    }
+
+    public void setMaxNumberOfPeople(int maxNumberOfPeople) {
+        this.maxNumberOfPeople = maxNumberOfPeople;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getMaxFuel() {
+        return maxFuel;
+    }
+
+    public void setMaxFuel(double maxFuel) {
+        this.maxFuel = maxFuel;
+    }
+
+    public double getCurrentFuel() {
+        return currentFuel;
+    }
+
+    public void setCurrentFuel(double currentFuel) {
+        this.currentFuel = currentFuel;
+    }
+
+    public double getMpg() {
+        return mpg;
+    }
+
+    public void setMpg(double mpg) {
+        this.mpg = mpg;
+    }
+
+    public boolean isTheCarOn() {
+        return isTheCarOn;
+    }
+
+    public void setTheCarOn(boolean theCarOn) {
+        isTheCarOn = theCarOn;
+    }
+
+    public char getCondition() {
+        return condition;
+    }
+
+    public void setCondition(char condition) {
+        this.condition = condition;
+    }
+
+    public String getNameOfCar() {
+        return nameOfCar;
+    }
+
+    public void setNameOfCar(String nameOfCar) {
+        this.nameOfCar = nameOfCar;
+    }
+
     public void getIn(){
         if(numberOfPeopleInCar < maxNumberOfPeople){
             numberOfPeopleInCar++;
@@ -61,6 +149,10 @@ public class Car {
             //Otherwise print out the error
             System.out.println("The car is already on " + isTheCarOn);
         }
+    }
+
+    public void upgradeMaxSpeed(){
+        setMaxSpeed(getMaxSpeed() +10);
     }
 
 
